c3706d66afcbf4418a7f13d1c41ebd8e6ca4e261
jperales64 <jaimeperales@rocketmail.com>
2021-12-16 17:22:27 -0600

    minor change

--- src/todoController.js
+++ src/todoController.js
@@ -2,8 +2,4 @@ import './style.css'
 import todoModel from './todoModel';
 import todoView from './todoView';
 
-todoModel.addTodoListItem("Eat", "Put food in mouth", "today", "high");
-todoModel.addTodoListItem("the", "Put food in mouth", "today", "high");
-todoModel.addTodoListItem("Pussy", "Put food in mouth", "today", "high");
-
 todoView.displayTodoList(todoModel.getTodoList());
