f88413a2ac652bf0f7be7269dcb328051fd2a9d0
jperales64 <jaimeperales@rocketmail.com>
2021-12-16 15:54:37 -0600

    created some divs

--- src/todoView.js
+++ src/todoView.js
@@ -1,13 +1,23 @@
 const todoView = (() => {
     const container = document.querySelector('.content');
 
+    const mainDiv = document.createElement('div');
+    mainDiv.classList.add('main-div');
+
+    const listDiv = document.createElement('div');
+    listDiv.classList.add('list-div');
+
+    mainDiv.appendChild(listDiv);
+    container.appendChild(mainDiv);
+
+
     const displayTodoList = (todoList) => {
         for (let listItem of todoList) {
             const listItemDiv = document.createElement('div');
             const listItemTitle = document.createElement('h3');
             listItemTitle.textContent = listItem.getTitle();
             listItemDiv.appendChild(listItemTitle);
-            container.appendChild(listItemDiv);
+            listDiv.appendChild(listItemDiv);
         }
     }
 
