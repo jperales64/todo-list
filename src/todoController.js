9a88a14b96330fd1f24c0901a13a6e27db24b153
jperales64 <jaimeperales@rocketmail.com>
2021-12-18 22:01:08 -0600

    added "add task" button

--- src/todoController.js
+++ src/todoController.js
@@ -2,5 +2,5 @@ import './style.css'
 import todoModel from './todoModel';
 import todoView from './todoView';
 
-todoModel.addTodoListItem("Dude", "Dude", "Dude", "Dude", "Dude");
+
 todoView.displayTodoList(todoModel.getTodoList());
--- src/todoView.js
+++ src/todoView.js
@@ -1,44 +1,53 @@
 const todoView = (() => {
-    const container = document.querySelector('.content');
+    const container = document.querySelector(".content");
 
-    const mainDiv = document.createElement('div');
-    mainDiv.classList.add('main-div');
+    const toolBar = document.createElement("div");
+    toolBar.classList.add("tool-bar");
 
-    const listDiv = document.createElement('div');
-    listDiv.classList.add('list-div');
+    const addButton = document.createElement("button");
+    addButton.classList.add("add-button");
+    addButton.textContent = "Create a Task";
+    toolBar.appendChild(addButton);
+
+    const toolBar1 = document.createElement("div");
+    toolBar1.classList.add("tool-bar");
+
+    const mainDiv = document.createElement("div");
+    mainDiv.classList.add("main-div");
+
+    const listDiv = document.createElement("div");
+    listDiv.classList.add("list-div");
 
     mainDiv.appendChild(listDiv);
+    container.appendChild(toolBar);
     container.appendChild(mainDiv);
-
 
     const displayTodoList = (todoList) => {
         for (let listItem of todoList) {
-            const listItemDiv = document.createElement('div');
-            const listItemTitle = document.createElement('h3');
+            const listItemDiv = document.createElement("div");
+            const listItemTitle = document.createElement("h3");
             listItemTitle.textContent = listItem.getTitle();
+
             listItemDiv.appendChild(listItemTitle);
             listDiv.appendChild(listItemDiv);
-            listItemDiv.classList.add('list-item-div');
+            listItemDiv.classList.add("list-item-div");
 
-            listItemDiv.addEventListener('mouseenter', function() {
-                this.classList.remove('list-item-div');
-                this.classList.add('hover');
-            })
-            listItemDiv.addEventListener('mouseleave', function() {
-                this.classList.add('list-item-div');
-                this.classList.remove('hover');
-            })
+            listItemDiv.addEventListener("mouseenter", function() {
+                this.classList.remove("list-item-div");
+                this.classList.add("hover");
+            });
+            listItemDiv.addEventListener("mouseleave", function() {
+                this.classList.add("list-item-div");
+                this.classList.remove("hover");
+            });
 
-            listItemDiv.addEventListener('click', function() {
-
-                this.classList.toggle('list-item-selected');
-            })
-
+            listItemDiv.addEventListener("click", function() {
+                this.classList.toggle("list-item-selected");
+            });
         }
-    }
+    };
 
     return { displayTodoList };
 })();
-
 
 export default todoView;
