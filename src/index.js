921a5c4ac127c860a8f33cf0bde7e1b8999c38f2
jperales64 <jaimeperales@rocketmail.com>
2021-12-23 22:26:54 -0600

    created todo item factory

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import todoItemFactory from "./todoItem";
---
+++ src/todoItem.js
@@ -0,0 +1,6 @@
+  const todoItemFactory = (title, description, dueDate, priority) => {
+
+      return { title, description, dueDate, priority }
+  };
+
+  export default todoItemFactory;
